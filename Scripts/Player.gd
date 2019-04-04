b29f56e7f72b7f4108986c99ce44b90ad319fed7
AngelPhenix <breathstylebro@gmail.com>
2019-04-05 00:06:34 +0200

    Added constructor & FSM

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,19 +1,63 @@
 extends KinematicBody2D
 class_name UnitController
 
+# MOVING VARIABLES
 var movement: Vector2
 var tile_size: int
 var is_moving: bool = false
 export var animation_speed: float = 1
 
+# GAME VARIABLES
+var attack: int = 1
+var defense: int = 1
+var total_movements: int = 3
+var is_selected: bool = false
+var turn_id: int
+var movements_left: int
+
+# FSM VARIABLES
+var state: int
+enum {IDLE, MOVING, SLEEPING, SELECTED}
+
 # Base the tile_size on the size of the texture used * sprite scale.
 func _ready() -> void:
 	tile_size = ($sprite as Sprite).texture.get_width() * ($sprite as Sprite).scale.x
+	state = SLEEPING
+
+# PRESS SPACE TO ACTIVATE THE UNIT
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		movements_left = total_movements
+		is_selected = true
 
 # Stops the inputs if the unit didn't finish its movement.
 func _physics_process(delta: float) -> void:
-	if !is_moving:
+	if is_selected:
 		movement_loop()
+	state_loop()
+
+func state_loop():
+	if state == IDLE && movement != Vector2():
+		change_state(MOVING)
+	if state == MOVING && movement == Vector2():
+		change_state(IDLE)
+	if state == MOVING && movements_left <= 1:
+		change_state(SLEEPING)
+	if state == SLEEPING && is_selected:
+		change_state(IDLE)
+
+func change_state(new_state: int) -> void:
+	state = new_state
+	match state:
+		IDLE:
+			($anim as AnimationPlayer).play("idle")
+		MOVING:
+			($tween as Tween).interpolate_property(self, "position", position, position + movement * tile_size, animation_speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			($tween as Tween).start()
+		SLEEPING:
+			($anim as AnimationPlayer).stop()
+			is_selected = false
+			print("animation stopped ! Go to unit having (turn_id +1) !")
 
 # unit not moving, get movement to execute tween with "animation_speed"(export) speed.
 func movement_loop() -> void:
@@ -35,19 +79,19 @@ func movement_loop() -> void:
 		movement = Vector2(-1, -1)
 	else:
 		movement = Vector2()
-	
-	# movement is different from (0,0) : Player moved, starting the tween.
-	if movement != Vector2():
-		($tween as Tween).interpolate_property(self, "position", position, position + (movement.normalized()) * tile_size, animation_speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-		($tween as Tween).start()
+
+#func create_unit(atk: int, def: int, moves: int):
+#	attack = atk
+#	defense = def
+#	total_movements = moves
 
 # Unit moving, stop flickering animation and inputs.
 func _on_tween_tween_started(object, key):
 	($sprite as Sprite).visible = true
 	($anim as AnimationPlayer).stop()
 	is_moving = true
+	movements_left -= 1
 
 # Unit finished moving : inputs are free and flickering animation restarts.
 func _on_tween_tween_completed(object: Object, key: NodePath) -> void:
-	($anim as AnimationPlayer).play("idle")
 	is_moving = false
